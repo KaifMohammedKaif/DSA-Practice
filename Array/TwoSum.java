f8c1879fdf7656b3e0023a6eaa68f06fe5004c1a
Mohammed Kaif <144600200+KaifMohammedKaif@users.noreply.github.com>
2024-03-17 23:20:34 +0530

    Update TwoSum.java

--- Array/TwoSum.java
+++ Array/TwoSum.java
@@ -2,21 +2,29 @@ import java.util.Arrays;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int [] arr ={2,7,7,12,15};
-        int tar = 14;
-        int [] answer = twoSum(arr, tar);
+        int[] arr = {2, 7, 7, 12, 15};
+        int target = 14;
+        int[] answer = twoSum(arr, target);
         System.out.println(Arrays.toString(answer));
     }
 
-    static int [] twoSum(int [] arr, int tar){
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                if (tar == (arr[i]+arr[j])) {
-                    return new int [] {i,j};
-                }
+    static int[] twoSum(int[] arr, int target) {
+        Arrays.sort(arr); // Sort the array
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == target) {
+                return new int[]{left, right};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
             }
         }
 
-        return new int[] {-1,-1};
+        return new int[]{-1, -1};
     }
 }
