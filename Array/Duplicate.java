d7fc780b4cbcb0999cd137055e31463e2c41d31a
Mohammed Kaif <144600200+KaifMohammedKaif@users.noreply.github.com>
2024-03-27 23:00:57 +0530

    Update Duplicate.java

--- Array/Duplicate.java
+++ Array/Duplicate.java
@@ -1,20 +1,18 @@
-/**
- * Duplicate
- */
+import java.util.Arrays;
+
 public class Duplicate {
     public static void main(String[] args) {
-        int [] arr = {1,2,3,1};
+        int[] arr = {1, 2, 3, 1};
         System.out.println(isDuplicate(arr));
     }
 
-    static boolean isDuplicate(int [] arr){
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                if (arr[i]==arr[j]) {
-                    return true;
-                }
+    static boolean isDuplicate(int[] arr) {
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                return true;
             }
         }
         return false;
     }
-}
+} 
