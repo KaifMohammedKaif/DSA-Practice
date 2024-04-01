29c89ddc95d0b2ee55780698f62d2fb4e22344db
Mohammed Kaif <144600200+KaifMohammedKaif@users.noreply.github.com>
2024-04-01 19:17:04 +0530

    Update patterns.java

--- pattern/patterns.java
+++ pattern/patterns.java
@@ -56,6 +56,30 @@ public class patterns {
         }
     }
 
+    static void pattern 13() {
+        int size = 7;
+        // Upper half of the diamond
+        for (int i = 1; i <= size; i += 2) {
+            for (int j = size; j > i; j -= 2) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+        // Lower half of the diamond
+        for (int i = size - 2; i >= 1; i -= 2) {
+            for (int j = size; j > i; j -= 2) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+
     static void pattern12() {
         int rows = 5;
         for (int i = 1; i <= rows; i++) {
